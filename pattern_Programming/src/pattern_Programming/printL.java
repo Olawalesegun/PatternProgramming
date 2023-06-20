c1e8ca46093ca4bcfb0aa70a49a373f40e9b2ef8
Olawalesegun <olawalesegun0@gmail.com>
2023-06-20 21:41:39 +0100

    Getting into some more complex pattern Programming, refactoring comes later

--- pattern_Programming/src/pattern_Programming/printL.java
+++ pattern_Programming/src/pattern_Programming/printL.java
@@ -36,6 +36,18 @@ public class printL {
 					System.out.print(" ");
 				}
 			}
+			System.out.print("  ");
+			for(int j=0; j<n; j++) {
+				if(i==0 && !(j==n-1)||
+						i>=1 && j==n-1 &&!(i==n-1 && j==n-1)||
+						i>=1 && j==0||
+						i==n-1 && j<n-1) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
 			System.out.println(" ");
 		}
 		
