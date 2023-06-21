e4f17a683acca8ea623f93b5bd3d9c4276c6cc76
Olawalesegun <olawalesegun0@gmail.com>
2023-06-21 22:55:17 +0100

    Making it cleaner and readable

--- pattern_Programming/src/pattern_Programming/printL.java
+++ pattern_Programming/src/pattern_Programming/printL.java
@@ -12,17 +12,7 @@ public class printL {
 			System.out.print("  ");
 			print.printThirdAlphabet(i, n);
 			System.out.print("  ");
-			for(int j=0; j<n; j++) {
-				if(i==0 && !(j==n-1)||
-						i>=1 && j==n-1 &&!(i==n-1 && j==n-1)||
-						i>=1 && j==0||
-						i==n-1 && j<n-1) {
-					System.out.print("*");
-				}
-				else {
-					System.out.print(" ");
-				}
-			}
+			print.printFourthAlphabet(i, n);
 			System.out.print("  ");
 			for(int j=0; j<n; j++) {
 				if(i==0 ||
@@ -71,6 +61,19 @@ public class printL {
 					i==n-1 && j>0) {
 				System.out.print("*");
 			}else {
+				System.out.print(" ");
+			}
+		}
+	}
+	public void printFourthAlphabet(int i, int n) {
+		for(int j=0; j<n; j++) {
+			if(i==0 && !(j==n-1)||
+					i>=1 && j==n-1 &&!(i==n-1 && j==n-1)||
+					i>=1 && j==0||
+					i==n-1 && j<n-1) {
+				System.out.print("*");
+			}
+			else {
 				System.out.print(" ");
 			}
 		}
