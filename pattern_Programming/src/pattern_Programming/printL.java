422ff1df80140364d96ac0ea871febb8aaf0166b
Olawalesegun <olawalesegun0@gmail.com>
2023-06-21 22:37:28 +0100

    Getting into some more complex pattern Programming, refactoring comes later

--- pattern_Programming/src/pattern_Programming/printL.java
+++ pattern_Programming/src/pattern_Programming/printL.java
@@ -1,18 +1,12 @@
 package pattern_Programming;
 
 public class printL {
+	public static int n = 10;
+	public static int i = 0;
 	public static void main(String[] args) {
-		int n = 10;
-		for (int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				if(i==0 && j>=1 && j<n-1 ||
-						i>0 && j==n-1 || 
-						i>0 && j==0 ||
-						i==(n-1)/2) {
-					System.out.print("*");
-				}
-				 else { System.out.print(" "); }	 
-			}
+		
+		for (i=0; i<n; i++) {
+			printFirstAlphabet(i, n);
 			System.out.print("   ");
 			for(int j=0; j<n; j++) {
 				if(i==0 && !(j==0) && !(j==n-1)||
@@ -63,5 +57,16 @@ public class printL {
 			System.out.println(" ");
 		}
 		
+	}
+	public static void printFirstAlphabet(int i, int n) {
+		for(int j=0; j<n; j++) {
+			if(i==0 && j>=1 && j<n-1 ||
+					i>0 && j==n-1 || 
+					i>0 && j==0 ||
+					i==(n-1)/2) {
+				System.out.print("*");
+			}
+			 else { System.out.print(" "); }	 
+		}
 	}
 }
